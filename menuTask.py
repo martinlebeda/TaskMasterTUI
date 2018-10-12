2ee93ccb574cd5c01c2c79205bf736091f7b4432
Martin Lebeda <lebeda@marbes.cz>
2018-10-12 07:18:22 +0200

    NEW: insert, edit and delete tasks

--- menuTask.py
+++ menuTask.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
+import os
 import re
 import subprocess
 import sys
 
-query = " | ".join(sys.argv[1:])
+EDIT_FILE = '/tmp/todo.txt'
+EDITOR = os.environ.get('EDITOR','vim')
 
+query = " | ".join(sys.argv[1:])
 
 # execute taskmaster task
 def exec_task(ids, *cmd):
@@ -57,12 +60,16 @@ def open_jira(names):
 # open_url(['https://www.databazeknih.cz/knihy/milenium-muz-ktery-hledal-svuj-stin-342724', 'http://github.com/tmux-plugins/tmux-open'])
 # exit(0)
 
+def edit_tasks(EDIT_FILE):
+    subprocess.call([EDITOR, EDIT_FILE])
+    subprocess.call(["tm", "tk", "import", EDIT_FILE])
+
 while True:
     '''Main loop'''
     p1 = subprocess.Popen(["tm", "tk", "-C", "ls"], stdout=subprocess.PIPE)
     p2 = subprocess.Popen(["fzf", '--multi', '--no-sort', '--border', "--ansi",
                            '--reverse', '--print-query', '--query=' + query,
-                           '--expect=ctrl-w,ctrl-p,ctrl-u,ctrl-j,ctrl-c,f5,f8,alt-a,alt-b,alt-c,alt-l'],
+                           '--expect=ctrl-w,ctrl-p,ctrl-u,ctrl-j,ctrl-c,f5,f8,alt-a,alt-b,alt-c,alt-l,f2,f4,ctrl-alt-d'],
                           stdin=p1.stdout, stdout=subprocess.PIPE)
     p1.wait()
     lines = p2.stdout.readlines()
@@ -83,6 +90,11 @@ while True:
     if key == 'f5':
         continue  # only refresh
 
+    if key == 'f2':
+        if os.path.exists(EDIT_FILE):
+            os.remove(EDIT_FILE)
+        edit_tasks(EDIT_FILE)
+
     ids = []
     names = []
     for line in lines:
@@ -99,6 +111,13 @@ while True:
     elif key == 'f8':
         print("mark defer ", taskId)
         exec_task(ids, "defer", "--context", "@defered")  # TODO Lebeda - konstantu za param
+    elif key == 'f4':
+        print("edit tasks", taskId)
+        exec_task(ids, "export", EDIT_FILE)
+        edit_tasks(EDIT_FILE)
+    elif key == 'ctrl-alt-d':
+        print("delete tasks", taskId)
+        exec_task(ids, "delete")
     elif key == 'ctrl-w':
         exec_task(ids, "work", "-w")
     elif key == 'alt-a':
